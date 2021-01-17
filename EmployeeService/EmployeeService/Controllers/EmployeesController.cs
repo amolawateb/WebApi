2a1b3c4291aae503d3aec43688d2473061dfe37e
Amol Awate <amolawateb@gmail.com>
2021-01-17 21:03:18 +0000

    #Web Api user registration

---
+++ EmployeeService/EmployeeService/Controllers/EmployeesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeService.Controllers
+{
+    [Authorize]
+    public class EmployeesController : ApiController
+    {
+        EmployeeDBEntities employeesDb = new EmployeeDBEntities();
+
+        [HttpGet]
+        public IEnumerable<Employee> GetEmployees()
+        {
+            EmployeeDBEntities employeesDb = new EmployeeDBEntities();
+            return employeesDb.Employees.ToList();
+        }
+    }
+}
